9e12feb9a145413843f767301c2fe626ea41fa91
mikel <mikelduke@mdp3.net>
2017-07-11 23:02:39 -0500

    remove ~ example to work with php directory iterator

--- gitManagerConfig.php
+++ gitManagerConfig.php
@@ -3,7 +3,7 @@ ini_set ( 'display_errors', 1 );
 
 error_reporting ( E_ALL );
 class GitSettings {
-	public static $_gitRepoPath = "~/git/";
+	public static $_gitRepoPath = "/opt/git/";
 	public static $_gitClonePrefix = "ssh://user@something/";
 	
 	public static $_adminEmails = array("email1@something.com");
